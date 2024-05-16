b2ca23d26d0c167b0e4f2b3bc5fcdf4ff863f0bc
fabiocalcacarvalho <fabiocalcacarvalho@gmail.com>
2024-05-16 08:05:18 -0300

    detentor automático

--- principal/forms.py
+++ principal/forms.py
@@ -3,7 +3,7 @@ from .models import Partida, Jogador
 class PartidaForm(forms.ModelForm):
     class Meta:
         model = Partida
-        fields = ['detentor_atual', 'desafiante', 'data', 'placar_detentor_atual', 'placar_desafiante', 'penaltis_detentor_atual', 'penaltis_desafiante']
+        fields = ['detentor_atual', 'desafiante', 'data', 'placar_detentor_atual', 'placar_desafiante', 'penaltis_detentor_atual', 'penaltis_desafiante']    
     def clean_placar_detentor_atual(self):
         placar = self.cleaned_data['placar_detentor_atual']
         if placar < 0:
@@ -15,3 +15,10 @@ class PartidaForm(forms.ModelForm):
         if placar < 0:
             raise forms.ValidationError('O placar deve ser positivo')
         return placar
+    
+    def __init__(self, *args, **kwargs):
+        super(PartidaForm, self).__init__(*args, **kwargs)
+        ultima_partida = Partida.objects.last()
+        if ultima_partida:
+            ultimo_vencedor = ultima_partida.determinar_vencedor().nome
+            self.fields['detentor_atual'].initial = Jogador.objects.filter(nome=ultimo_vencedor).first()
