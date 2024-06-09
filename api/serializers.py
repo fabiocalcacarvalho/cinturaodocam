de2513b41d7c259c35a884ab0879c479e84dbf14
fabiocalcacarvalho <fabiocalcacarvalho@gmail.com>
2024-06-09 16:10:00 -0300

    correcao na api

--- api/serializers.py
+++ api/serializers.py
@@ -1,10 +1,17 @@
 from rest_framework import serializers
 from principal.models import Partida, Jogador
 
-class PartidaSerializer(serializers.ModelSerializer):
+class PartidaSerializer(serializers.Serializer):
+    id = serializers.IntegerField(read_only=True)
+    detentor_atual = serializers.CharField(max_length=100)
+    desafiante = serializers.CharField(max_length=100)
+    data = serializers.DateField()
+    placar_detentor_atual = serializers.IntegerField()
+    placar_desafiante = serializers.IntegerField()
+    penaltis_detentor_atual = serializers.IntegerField()
+    penaltis_desafiante = serializers.IntegerField()
     class Meta:
         model = Partida
-        fields = '__all__'
 
 class JogadorSerializer(serializers.ModelSerializer):
     class Meta:
